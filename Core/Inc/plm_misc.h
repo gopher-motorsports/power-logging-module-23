a97e7748e73287f9bfc87b5a2f281f40d9788516
olale013 <95550079+olale013@users.noreply.github.com>
2023-05-14 21:02:40 -0500

    move logging metrics to plm_misc

---
+++ Core/Inc/plm_misc.h
@@ -0,0 +1,13 @@
+/*
+ * plm_misc.h
+ *
+ *  Created on: May 14, 2023
+ *      Author: jonathan
+ */
+
+#ifndef INC_PLM_MISC_H_
+#define INC_PLM_MISC_H_
+
+void plm_update_logging_metrics(void);
+
+#endif /* INC_PLM_MISC_H_ */
--- Core/Src/plm.c
+++ Core/Src/plm.c
@@ -20,6 +20,7 @@
 #include "plm_sim.h"
 #include "plm_data.h"
 #include "plm_power.h"
+#include "plm_misc.h"
 
 // we might need to turn this up for launch control
 #define CAN_MESSAGE_FORWARD_INTERVAL_ms 50
@@ -101,41 +102,7 @@ void plm_heartbeat(void) {
         last_blink = tick;
     }
 
-    static uint32_t last_update = 0;
-    if (tick - last_update >= PLM_DELAY_LOGGING_METRICS) {
-        packetsDropped_ul.info.last_rx = tick;
-        packetsLogged_ul.info.last_rx = tick;
-
-        // SD and Xbee buffer fill %
-        storageBufferFill_percent.data = (float) SD_DB.buffers[SD_DB.write_index]->fill / SD_DB.buffers[SD_DB.write_index]->size * 100.0f;
-        telemetryBufferFill_percent.data = (float) XB_DB.buffers[XB_DB.write_index]->fill / XB_DB.buffers[XB_DB.write_index]->size * 100.0f;
-        storageBufferFill_percent.info.last_rx = tick;
-        telemetryBufferFill_percent.info.last_rx = tick;
-
-        // CAN bus load
-        // does not include messages filtered out by hardware
-        // does not include messages sent by the PLM
-        // assumes all CAN frames are 125 bits (11-bit ID, 8 data bytes, other frame stuff)
-        float hcan1_rx_bps = (hcan1_rx_callbacks * 125) / ((float)(tick - last_update) / 1000.0f);
-        float hcan2_rx_bps = (hcan2_rx_callbacks * 125) / ((float)(tick - last_update) / 1000.0f);
-        float hcan3_rx_bps = (hcan3_rx_callbacks * 125) / ((float)(tick - last_update) / 1000.0f);
-
-        // bus load = sampled bps / 1Mbps
-        can0Utilization_percent.data = hcan1_rx_bps / 1000000.0f * 100.0f;
-        can1Utilization_percent.data = hcan2_rx_bps / 1000000.0f * 100.0f;
-        can2Utilization_percent.data = hcan3_rx_bps / 1000000.0f * 100.0f;
-
-        can0Utilization_percent.info.last_rx = tick;
-        can1Utilization_percent.info.last_rx = tick;
-        can2Utilization_percent.info.last_rx = tick;
-
-        hcan1_rx_callbacks = 0;
-        hcan2_rx_callbacks = 0;
-        hcan3_rx_callbacks = 0;
-        last_update = tick;
-    }
-
-    // blink any active errors
+    plm_update_logging_metrics();
     plm_err_blink();
 
     osDelay(PLM_TASK_DELAY_HEARTBEAT);
